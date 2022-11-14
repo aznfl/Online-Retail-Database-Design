a5b220768298d283e40f8750699051486a5eba82
aznf.lubis <arlyzeina24@gmail.com>
2022-11-15 01:21:38 +0700

    insert

--- 16-insert_tb_detail_bill.sql
+++ 16-insert_tb_detail_bill.sql
@@ -3,4 +3,11 @@ INSERT INTO detail_bill
 VALUES
 (1, 'bil01', 'cus04', 'buy01'),
 (2, 'bil01', 'cus04', 'buy02'), 
-(3, 'bil01', 'cus04', 'buy03');
+(3, 'bil01', 'cus04', 'buy03'),
+(4, 'bil02', 'cus01', 'buy04'),
+(5, 'bil03', 'cus06', 'buy05'),
+(6, 'bil03', 'cus09', 'buy06'),
+(7, 'bil04', 'cus02', 'buy07'),
+(8, 'bil05', 'cus03', 'buy08'),
+(9, 'bil05', 'cus03', 'buy09'),
+(10, 'bil05', 'cus03', 'buy10');
